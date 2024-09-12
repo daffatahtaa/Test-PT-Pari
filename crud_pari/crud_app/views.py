ca06bdb0e1ff2f09e34b6c397756adc3e88fc1d0
daffatahtaa <daffatahta12@gmail.com>
2024-09-12 15:52:49 +0700

    updated app view

--- crud_pari/crud_app/views.py
+++ crud_pari/crud_app/views.py
@@ -2,4 +2,40 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Item
+from .serializer import ItemSerializer
 
+@api_view(['GET'])
+def get_items(request):
+    items = Item.objects.all()
+    serializer = ItemSerializer(items, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def get_item(request, id):
+    item = Item.objects.get(id=id)
+    serializer = ItemSerializer(item)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def create_item(request):
+    serializer = ItemSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response (serializer.errors, status=400)
+
+@api_view(['PUT'])
+def update_item(request, id):
+    item = Item.objects.get(id=id)
+    serializer = ItemSerializer(item, data = request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors, status=400)
+
+@api_view(['DELETE'])
+def delete_item (request, id):
+    item = Item.objects.get(id=id)
+    item.delete()
+
+    return Response(status=204)
