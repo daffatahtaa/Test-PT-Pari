910ee98da866ed167f65b4820dcffb66f400836b
daffatahtaa <daffatahta12@gmail.com>
2024-09-12 15:44:24 +0700

    created model

--- crud_pari/crud_app/views.py
+++ crud_pari/crud_app/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Item
 
-# Create your views here.
