83514511e0caabd667b6d7b9fc8715947c3dbbe4
daffatahtaa <daffatahta12@gmail.com>
2024-09-12 16:15:07 +0700

    fixing urls typo

--- crud_pari/crud_app/urls.py
+++ crud_pari/crud_app/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-urlpatters = [
+urlpatterns = [
     path('items/', views.get_items),
     path('items/<int:id>/', views.get_item),
     path('items/create/', views.create_item),
