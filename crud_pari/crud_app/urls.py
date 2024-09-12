c931750938fd5a7858ae12a2e4869adc295fbb76
daffatahtaa <daffatahta12@gmail.com>
2024-09-12 15:55:19 +0700

    update app urls

---
+++ crud_pari/crud_app/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatters = [
+    path('items/', views.getitems),
+    path('items/<int:id>/', views.get_item),
+    path('items/create/', views.create_item),
+    path('items/<int:id>/update/', views.update_item),
+    path('items/<int:id>/delete/', views.delete_item),
+    
+]
